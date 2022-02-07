840ac4e1ccb1497627ccdcd7387936bce3024500
dibbyoch <85834552+dibbyoch@users.noreply.github.com>
2022-02-07 13:57:49 +0530

    Create 2_tablecreation.sql

---
+++ 2_tablecreation.sql
@@ -0,0 +1,180 @@
+CREATE TABLE EmployeesTable (EMP_ID int(6) NOT NULL,
+FIRST_NAME char(13) NOT NULL,
+LAST_NAME char(13) NOT NULL,
+SALARY int(7),
+JOINING_DATE DATETIME,
+DEPARTMENT char(10));
+
+INSERT INTO EmployeesTable (EMP_ID , FIRST_NAME ,
+LAST_NAME ,
+SALARY ,
+JOINING_DATE,
+DEPARTMENT )
+VALUES
+ ('001','Manish','Agarwal','700000','2019-04-20 09:00:00',
+'HR');
+
+
+
+INSERT INTO EmployeesTable (EMP_ID , FIRST_NAME ,
+LAST_NAME ,
+SALARY ,
+JOINING_DATE,
+DEPARTMENT )
+VALUES
+ ('002','Niranjan','Bose','20000', '2019-02-11 09:00:00'
+,'DA');
+
+
+
+INSERT INTO EmployeesTable (EMP_ID , FIRST_NAME ,
+LAST_NAME ,
+SALARY ,
+JOINING_DATE,
+DEPARTMENT )
+VALUES
+ ('003', 'Vivek', 'Singh', '100000' ,'2019-01-20 09:00:00',
+'DA');
+
+
+
+INSERT INTO EmployeesTable (EMP_ID , FIRST_NAME ,
+LAST_NAME ,
+SALARY ,
+JOINING_DATE,
+DEPARTMENT )
+VALUES
+ ('004', 'Asutosh', 'Kapoor', '700000', '2019-03-20 09:00:00'
+,'HR');
+
+
+
+INSERT INTO EmployeesTable (EMP_ID , FIRST_NAME ,
+LAST_NAME ,
+SALARY ,
+JOINING_DATE,
+DEPARTMENT )
+VALUES
+ ('005', 'Vihaan', 'Banerjee', '300000', '2019-06-11 09:00:00'
+,'DA');
+
+
+
+
+INSERT INTO EmployeesTable (EMP_ID , FIRST_NAME ,
+LAST_NAME ,
+SALARY ,
+JOINING_DATE,
+DEPARTMENT )
+VALUES
+ ('006' ,'Atul', 'Diwedi', '400000', '2019-05-11 09:00:00'
+,'Account');
+
+
+
+INSERT INTO EmployeesTable (EMP_ID , FIRST_NAME ,
+LAST_NAME ,
+SALARY ,
+JOINING_DATE,
+DEPARTMENT )
+VALUES
+ ('007','Satyendra', 'Tripathi', '95000','2019-03-20 09:00:00'
+,'Account');
+
+
+
+
+INSERT INTO EmployeesTable (EMP_ID , FIRST_NAME ,
+LAST_NAME ,
+SALARY ,
+JOINING_DATE,
+DEPARTMENT )
+VALUES
+ ('008', 'Pritika', 'Bhatt','80000' ,'2019-02-11 09:00:00','DA');
+
+select * from EmployeesTable;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE VariablesDetails (
+    EMP_REF_ID int(6),
+    VARIABLES_DATE DATETIME,
+    VARIABLES_AMOUNT int(12) );
+
+
+
+INSERT INTO VariablesDetails (EMP_REF_ID,  VARIABLES_DATE,  VARIABLES_AMOUNT )
+VALUES
+ ('1', '2019-02-20 00:00:00','15000');
+
+INSERT INTO VariablesDetails (EMP_REF_ID,  VARIABLES_DATE,  VARIABLES_AMOUNT )
+VALUES
+ ('2', '2019-06-11 00:00:00', '30000');
+
+INSERT INTO VariablesDetails (EMP_REF_ID,  VARIABLES_DATE,  VARIABLES_AMOUNT )
+VALUES
+ ('3', '2019-02-20 00:00:00', '42000');
+
+INSERT INTO VariablesDetails (EMP_REF_ID,  VARIABLES_DATE,  VARIABLES_AMOUNT )
+VALUES
+ ('4','2019-02-20 00:00:00', '14500');
+
+
+INSERT INTO VariablesDetails (EMP_REF_ID,  VARIABLES_DATE,  VARIABLES_AMOUNT )
+VALUES
+ ('5', '2019-06-11 00:00:00', '23500');
+
+CREATE TABLE DesignationTable (EMP_REF_ID int(6) ,EMP_TITLE char(15) ,
+AFFECTED_FROM DATETIME);
+
+INSERT INTO DesignationTable (EMP_REF_ID  ,EMP_TITLE ,
+AFFECTED_FROM 
+ )
+VALUES
+ ('1', 'Asst. Manager', '2019-02-20 00:00:00');
+
+INSERT INTO DesignationTable (EMP_REF_ID ,EMP_TITLE,
+AFFECTED_FROM )
+VALUES
+ ('2', 'Senior Analyst', '2019-01-11 00:00:00');
+
+INSERT INTO DesignationTable (EMP_REF_ID,EMP_TITLE  ,
+AFFECTED_FROM )
+VALUES
+ ('8' ,'Senior Analyst', '2019-04-06 00:00:00');
+
+INSERT INTO DesignationTable (EMP_REF_ID  ,EMP_TITLE  ,
+AFFECTED_FROM )
+VALUES
+ ('5', 'Manager', '2019-10-06 00:00:00');
+
+INSERT INTO DesignationTable (EMP_REF_ID  ,EMP_TITLE  ,
+AFFECTED_FROM)
+VALUES
+ ('4' ,'Asst. Manager', '2019-12-06 00:00:00');
+
+INSERT INTO DesignationTable (EMP_REF_ID  ,EMP_TITLE ,
+AFFECTED_FROM  )
+VALUES
+ ('7', 'Team Lead', '2019-06-06 00:00:00');
+
+INSERT INTO DesignationTable (EMP_REF_ID ,EMP_TITLE ,
+AFFECTED_FROM )
+VALUES
+ ('6', 'Team Lead', '2019-09-06 00:00:00');
+
+INSERT INTO DesignationTable (EMP_REF_ID ,EMP_TITLE ,
+AFFECTED_FROM )
+VALUES
+ ('3', 'Senior Analyst', '2019-08-06 00:00:00');
